9b5e1b5a56fd9961992775bc7c2c8a98f2c0644e
Lennin Ibarra <ing.lenninibarra@gmail.com>
2021-08-09 19:08:43 -0500

    Agregando nodos intermedios

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -68,7 +68,44 @@ class MySinglyLinkedList {
 
     return this;
   }
+
+  //Agregando nodos intermedios
+  //controlar el garbash collector
+  //asegurar  que todos los nodos queden con un pointer
+  insert(index, value) {
+    //si el valor del indice es mayor a la longitud lo agrega de último
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    //primer pointer guarda el pointer del elemento anterior a donde vamos a insertar
+    const firstPointer = this.getTheIndex(index - 1);
+    //es el segundo pointer, el nodo que va después del que vamos a agregar
+    const holdingPointer = firstPointer.next;
+    //unimos el first pointer con el nuevo nodo
+    firstPointer.next = newNode;
+    //unimos el nuevo nodo con el segundo pointer
+    newNode.next = holdingPointer;
+
+    this.length++;
+    return this;
+  }
+
+  //Metodo para buscar un nodo
+  getTheIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
+
+
 
 let myLinkedList = new MySinglyLinkedList(1);
 
@@ -78,4 +115,5 @@ myLinkedList.append(3);
 myLinkedList.append(4);
 myLinkedList.append(5);
 myLinkedList.prepend(0);
+myLinkedList.insert(2, 5);
 console.log(myLinkedList);
